7966edfb74d3e6699b6f705f6fe8439c7136869f
El-Mokhtar-El-Malihi <134855413+El-Mokhtar-El-Malihi@users.noreply.github.com>
2023-07-23 01:22:30 +0100

    Update flags.c

--- flags.c
+++ flags.c
@@ -3,8 +3,8 @@
 /**
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * @i: take a parameters.
+ * Return: Flags: it should be allwod in draft
  */
 int get_flags(const char *format, int *i)
 {
