e1a31fe6b1cb86a50844635870957c057015ec33
El-Mokhtar-El-Malihi <134855413+El-Mokhtar-El-Malihi@users.noreply.github.com>
2023-07-23 01:25:09 +0100

    Update print_function.c

--- print_function.c
+++ print_function.c
@@ -7,7 +7,7 @@
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: Width
+ * @width: Widthit should be allwod in draft
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
