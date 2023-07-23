c50fe0345192e52f71b7d02c7aa46e3766429e97
El-Mokhtar-El-Malihi <134855413+El-Mokhtar-El-Malihi@users.noreply.github.com>
2023-07-23 01:23:33 +0100

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -5,6 +5,7 @@
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
+ * it should be allwod in draft
  *
  * Return: width.
  */
